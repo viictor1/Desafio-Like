04baa2167e49e5675374a330d55349908c943fd7
Victor <victorgaspalmeida@hotmail.com>
2024-02-22 18:16:17 -0300

    feat: addOrcamento in OrcamentoController

---
+++ src/main/java/com/desafiolike/demo/controller/OrcamentoController.java
@@ -0,0 +1,22 @@
+package com.desafiolike.demo.controller;
+
+import com.desafiolike.demo.entity.Orcamento;
+import com.desafiolike.demo.service.OrcamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/orcamento")
+public class OrcamentoController {
+
+    @Autowired
+    private OrcamentoService orcamentoService;
+
+    @PostMapping
+    public void addOrcamento(Orcamento orcamento){
+        orcamentoService.addOrcamento(orcamento);
+    }
+    
+}
