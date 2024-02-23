04b3434911ad9ceec29f83ae296a7730bc992121
Victor <victorgaspalmeida@hotmail.com>
2024-02-23 09:57:21 -0300

    refactor: changing return in controller

--- src/main/java/com/desafiolike/demo/controller/OrcamentoController.java
+++ src/main/java/com/desafiolike/demo/controller/OrcamentoController.java
@@ -3,9 +3,13 @@ package com.desafiolike.demo.controller;
 import com.desafiolike.demo.dto.OrcamentoDto;
 import com.desafiolike.demo.service.OrcamentoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @Controller
 @RequestMapping("/orcamento")
@@ -16,7 +20,13 @@ public class OrcamentoController {
 
     @PostMapping
     public void addOrcamento(OrcamentoDto orcamento){
-        orcamentoService.addOrcamento(orcamento);
+
+        orcamento = orcamentoService.addOrcamento(orcamento);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id")
+                .buildAndExpand(orcamento.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(orcamento);
     }
 
 }
