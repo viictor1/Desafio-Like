f521e576e7fe45a332c5d74451d8e1ed81a6d3bc
Victor <victorgaspalmeida@hotmail.com>
2024-02-25 20:36:36 -0300

    docs: adding swagger

--- backend/src/main/java/com/desafiolike/demo/controller/OrcamentoController.java
+++ backend/src/main/java/com/desafiolike/demo/controller/OrcamentoController.java
@@ -2,6 +2,11 @@ package com.desafiolike.demo.controller;
 
 import com.desafiolike.demo.dto.OrcamentoDto;
 import com.desafiolike.demo.service.OrcamentoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -21,6 +26,13 @@ public class OrcamentoController {
     @Autowired
     private OrcamentoService orcamentoService;
 
+    @Operation(summary = "Adicionar Orcamento", description = "Enviar orcamento para ser salvo no banco de dados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Sucesso",
+                    content =  @Content(schema = @Schema(implementation = OrcamentoDto.class))),
+            @ApiResponse(responseCode = "500", description = "Erro no sistema",
+                    content = @Content(schema = @Schema(implementation = Void.class))),
+    })
     @PostMapping
     public ResponseEntity<OrcamentoDto> addOrcamento(@RequestBody OrcamentoDto orcamento){
         orcamento = orcamentoService.addOrcamento(orcamento);
@@ -31,6 +43,13 @@ public class OrcamentoController {
         return ResponseEntity.created(uri).body(orcamento);
     }
 
+    @Operation(summary = "Selecionar Orcamentos", description = "Retorna todos os orcamentos em formato JSON")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Sucesso",
+                    content =  @Content(schema = @Schema(implementation = OrcamentoDto.class))),
+            @ApiResponse(responseCode = "500", description = "Erro no sistema",
+                    content = @Content(schema = @Schema(implementation = Void.class))),
+    })
     @GetMapping
     public ResponseEntity<List<OrcamentoDto>> getOrcamentos(){
         List<OrcamentoDto> orcamentos = orcamentoService.getOrcamentos();
